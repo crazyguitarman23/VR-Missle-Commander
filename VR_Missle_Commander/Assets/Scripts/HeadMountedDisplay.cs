5f2573c293493547d8cee7ae4aaea07c3fcca1a1
Jared Rowe <crazyguitarman23@gmail.com>
2017-01-07 23:20:43 -0800

    Basic Controls

---
+++ VR_Missle_Commander/Assets/Scripts/HeadMountedDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadMountedDisplay : MonoBehaviour 
+{
+	private SteamVR_TrackedObject hmd;
+
+	// Use this for initialization
+	void Start () 
+	{
+		hmd = GetComponent<SteamVR_TrackedObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
---
+++ VR_Missle_Commander/Assets/Scripts/InteractableItem.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableItem : MonoBehaviour 
+{
+	protected Rigidbody rigidbody;
+
+	protected bool currentlyInteracting;
+
+	private float rotationFactor = 60f;
+	private Quaternion rotationDelta;
+	private float angle;
+	private Vector3 axis;
+
+	private WandController attachedWand;
+
+	protected void Start () 
+	{
+
+		rigidbody = GetComponent<Rigidbody> ();
+		
+	}
+
+	protected void Update () 
+	{
+		if (attachedWand && currentlyInteracting)
+		{
+			rotationDelta = attachedWand.transform.FindChild("Rotation Offset").transform.rotation * Quaternion.Inverse (transform.rotation);
+			rotationDelta.ToAngleAxis (out angle, out axis);
+
+			if (angle > 180)
+			{
+				angle -= 360;
+			}
+
+			this.rigidbody.angularVelocity = (Time.fixedDeltaTime * angle * axis) * rotationFactor;
+		}
+	}
+
+	public void BeginInteraction(WandController wand)
+	{
+		attachedWand = wand;
+
+		currentlyInteracting = true;
+	}
+
+	public void EndInteraction(WandController wand)
+	{
+		if (wand == attachedWand) 
+		{
+			attachedWand = null;
+			currentlyInteracting = false;
+			this.rigidbody.angularVelocity = Vector3.zero;
+		}
+	}
+
+	public bool IsInteracting()
+	{
+		return currentlyInteracting;
+	}
+}
